d0237b9507e6050ae1423d954de642cb1e022030
Yarick Levchenco <jaroslav.levchenco11@gmail.com>
2022-10-31 14:21:28 +0200

    Фикс кривого слияния

--- Application/DataAccess.cs
+++ Application/DataAccess.cs
@@ -2,7 +2,7 @@
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+//using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
