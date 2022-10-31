fc58c33ea2071cfba7218ab517f648db505e1eb2
Yarick Levchenco <jaroslav.levchenco11@gmail.com>
2022-10-31 14:38:00 +0200

    + дизайн главного меню

---
+++ Application/MVVW/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Application.Core;
+
+namespace Application.MVVW.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ProfileViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM{ get; set; }
+        public ProfileViewModel ProfileVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();            
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            ProfileVM = new ProfileViewModel();
+            CurrentView = ProfileVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            ProfileViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ProfileVM;
+            });
+        }
+    }
+}
