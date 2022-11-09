5e3dab8089716af265eb52ecec1b81670a930f04
iAmBurmik <104519258+iAmBurmik@users.noreply.github.com>
2022-11-09 15:06:11 +0200

    Додавання фотографії

--- Application/MVVW/View/ProfileView.xaml.cs
+++ Application/MVVW/View/ProfileView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.MVVW.ViewModel;
+using MaterialDesignThemes.Wpf;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Application/MVVW/View/SettingsView.xaml.cs
+++ Application/MVVW/View/SettingsView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace Application.MVVW.View
 {
@@ -23,6 +26,31 @@ namespace Application.MVVW.View
         public SettingsView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { ValidateNames = true, Filter = "PNG|*.png" };
+            if (ofd.ShowDialog() == true)
+            {
+                FileInfo fi = new FileInfo(ofd.FileName);
+                string pathNewAvatar = fi.FullName;
+
+                //string pathOldAvatar = Path.GetFullPath("Images");
+                //var strIndex = pathOldAvatar.IndexOf("bin");
+                //pathOldAvatar = pathOldAvatar.Remove(strIndex, 10);
+                //File.Delete(pathOldAvatar);
+                //File.Move(pathNewAvatar, pathOldAvatar);
+
+                string pathOldAvatar = Path.GetFullPath("Images");
+                var strIndex = pathOldAvatar.IndexOf("bin");
+                pathOldAvatar = pathOldAvatar.Remove(strIndex, 10);
+                pathOldAvatar = pathOldAvatar + @"\avatar.png";
+                //string pathOldAvatar = @"D:\Документы\Никита\Универ\Курсовой проект ООП\OOP\OOP\Application\Images\avatar.png";
+                //string path = @"D:\Документы\Никита\Универ\Курсовой проект ООП\OOP\OOP\Application\Images";
+                File.Delete(pathOldAvatar);
+                File.Move(pathNewAvatar, pathOldAvatar);
+            }
         }
     }
 }
